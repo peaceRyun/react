dc97a13334fbe896651bc1ff83e84c2ec2cd4dcc
peaceRyun <dbsrn1110@gmail.com>
2025-02-04 12:31:22 +0900

    14-3

--- project1_step1_f/src/ui/Button1.jsx
+++ project1_step1_f/src/ui/Button1.jsx
@@ -1,17 +1,17 @@
 import styled from 'styled-components';
 
 const ButtonWrap = styled.button`
-    width: ${(props) => props.width};
+    width: ${(props) => props.$width};
     padding-top: 10px 0;
     border: none;
-    background-color: ${(props) => props.bgColor};
+    background-color: ${(props) => props.$bgColor};
     font-size: 20px;
     margin-left: 10px;
 `;
 // 자주 사용하는 작은 조각들을 UI에 설정하기
 // button, input, next버튼, prev 버튼 등
-const Button = (props) => {
+const Button1 = (props) => {
     return <ButtonWrap {...props}>{props.title || '버튼확인'}</ButtonWrap>;
 };
 
-export default Button;
+export default Button1;
